26a777ba1acce53ecfe7f168f08c82b993ac5ada
coronado03 <sebascoronado03@hotmail.com>
2022-08-15 17:41:13 -0600

    Added a toast system

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,43 @@
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 export default function Home() {
+  const notify = () =>
+    toast("Fullscreen is recommended for a good experience!");
+
+  useEffect(() => {
+    notify();
+  }, []);
+
   return (
     <>
-            <div className="portfolio-background1" />
-            <div className="portfolio-background2" />
-            <div className="portfolio-background3" />
-            <div className="portfolio-background4" />
-            <div className="portfolio-background5 relative">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          className: "",
+          style: {
+            border: "2px solid #174CF9",
+            padding: "16px",
+            color: "#FF2EDD",
+          },
+        }}
+      />
+      <div className="portfolio-background1" />
+      <div className="portfolio-background2" />
+      <div className="portfolio-background3" />
+      <div className="portfolio-background4" />
+      <div className="portfolio-background5 relative">
+        <div className="absolute top-[60 %] left-[25.2%] max-w-[17%]">
+          <img className="contain" src="OJITO-.gif" />
+        </div>
+      </div>
 
-              <div className="absolute top-[60%] left-[25.2%] max-w-[17%]">
-                <img className="contain" src="OJITO-.gif"/>
-              </div>
-              
-            </div>
-
-            <div className="portfolio-background6">
-              <div>
-                <img src="magical.gif"/>
-
-              </div>
-            </div>
-            <div className="portfolio-background7" />
-
+      <div className="portfolio-background6">
+        <div>
+          <img src="magical.gif" />
+        </div>
+      </div>
+      <div className="portfolio-background7" />
     </>
-  )
+  );
 }
